2130fee8456c9ebad27a354546f28754febecd7a
Shawn King <shking@hsc.wvu.edu>
2019-05-29 17:32:41 -0400

    FTR - Working to add JS files thru GIT to point to Big Cartel - Updated the Product page to show thumbnails and large images - Adjusted navigation to allow for mission page etc

---
+++ assets/scripts/product-slideshow.js
@@ -0,0 +1,10 @@
+﻿function show(nr) {
+    document.getElementById("table1").style.display = "none";
+    document.getElementById("table2").style.display = "none";
+    document.getElementById("table3").style.display = "none";
+    document.getElementById("table4").style.display = "none";
+    document.getElementById("table" + nr).style.display = "block";
+    //
+    var slides = document.getElementsByClassName("product__slide");
+    slides.className += "hidden";
+}
