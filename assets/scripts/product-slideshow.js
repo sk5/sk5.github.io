59ad5be9981897afae943aa477103e69f1a4e2e9
Shawn King <srk6992@gmail.com>
2019-05-29 21:56:32 -0400

    FTR - JS code adjustments to increment product__slide elements

--- assets/scripts/product-slideshow.js
+++ assets/scripts/product-slideshow.js
@@ -1,10 +1,18 @@
-﻿function show(nr) {
-    document.getElementById("table1").style.display = "none";
-    document.getElementById("table2").style.display = "none";
-    document.getElementById("table3").style.display = "none";
-    document.getElementById("table4").style.display = "none";
-    document.getElementById("table" + nr).style.display = "block";
+﻿window.onload = function () {
+    var list = document.getElementsByClassName("product__slide");
+    for (var i = 0; i < list.length; i++) {
+        list[i].setAttribute("data-slide", "image" + i);
+    }
+};
+
+function show(nr) {
+    //document.getElementById("table1").style.display = "none";
+    //document.getElementById("table2").style.display = "none";
+    //document.getElementById("table3").style.display = "none";
+    //document.getElementById("table4").style.display = "none";
+    //document.getElementById("table" + nr).style.display = "block";
     //
     var slides = document.getElementsByClassName("product__slide");
     slides.className += "hidden";
+    var currentSlide = slides.getAttribute('data-fruit');
 }
