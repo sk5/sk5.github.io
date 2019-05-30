d41a3ad0b5bf7faca7392d6fe813766179750d7c
Shawn King <shking@hsc.wvu.edu>
2019-05-30 18:27:10 -0400

    FTR - JS work to get product galleries to work

--- assets/scripts/product-slideshow.js
+++ assets/scripts/product-slideshow.js
@@ -2,6 +2,44 @@
     // Auto increment main images
     var list = document.getElementsByClassName("product__slide");
     for (var i = 0; i < list.length; i++) {
+        list[i].setAttribute("data-slide", "image-" + i);
+    }
+    //
+    // Auto increment thumbnails
+    var thumbnailList = document.getElementsByClassName("product__thumbnail--link");
+    for (var j = 0; j < thumbnailList.length; j++) {
+        thumbnailList[j].setAttribute("data-thumb", + j);
+    }
+};
+
+var buttons = document.getElementsByClassName("product__thumbnail--link"); // target all buttons into a element array/collection
+var buttonsCount = buttons.length; // cache the length
+for (var h = 0; h < buttonsCount; h++) { // reset the counter; check if its smaller than the array length; add itself
+    //console.log("before button");
+    buttons[h].onclick = function (e) { // assign a function to the onclick event
+        //console.log("here");
+        var imageName = this.getAttribute("data-thumb"); // alert the id attribute of the element clicked 
+        var element = document.querySelector("[data-slide='image-" + imageName + "']");
+        //console.log(element);
+        //
+        //
+        var slides = document.getElementsByClassName("product__slide");
+        for (var i = 0; i < slides.length; i++) {
+            var slide = slides[i];
+            slide.classList.remove("active");
+        }
+        //
+        element.classList.add("active");
+    };
+}
+//
+//
+//
+/*
+window.onload = function () {
+    // Auto increment main images
+    var list = document.getElementsByClassName("product__slide");
+    for (var i = 0; i < list.length; i++) {
         list[i].setAttribute("data-slide", "image" + i);
     }
     //
@@ -21,5 +59,11 @@ function show(nr) {
     //
     var slides = document.getElementsByClassName("product__slide");
     slides.className += "hidden";
-    var currentSlide = slides.getAttribute('data-fruit');
+    document.querySelectorAll("[data-slide='image'" + nr).className.remove("hidden");
+    //
+    var thumbnails = document.getElementsByClassName("product__thumbnail--link");
+    thumbnails.className.remove("active");
+    //var currentSlide = slides.getAttribute('data-fruit');
+    this.className += "active";
 }
+*/
